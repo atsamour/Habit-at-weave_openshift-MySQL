ef012d5287de3d51c7fe60d309298bde071b5875
atsamour <arkantos_gr@yahoo.gr>
2015-08-17 15:48:25 +0300

    test DB

--- src/main/java/com/atsamour/habitatweave/HelloServlet.java
+++ src/main/java/com/atsamour/habitatweave/HelloServlet.java
@@ -16,10 +16,22 @@ import javax.servlet.annotation.WebServlet;
 public class HelloServlet extends HttpServlet {
 
     @Override
+
+
+  private static final String DB_USERNAME = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+  private static final String DB_PASSWORD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+  private static final String DB_DATABASE = "mysql";
+  private static final String DB_HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+  private static final String DB_PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+
     protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
             throws ServletException, IOException {
         ServletOutputStream out = response.getOutputStream();
         out.write("Hello from servlet".getBytes());
+        out.write( ("\n " + DB_HOST + " " +DB_PORT) .getBytes());
+        out.write("Hello from servlet".getBytes());
+        out.write("Hello from servlet".getBytes());
+
         out.flush();
         out.close();
     }
